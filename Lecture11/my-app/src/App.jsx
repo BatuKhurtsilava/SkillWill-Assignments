feb6bc078269414bd910ef0bed6c3940159188e5
Batu Khurtsilava <124504221+BatuKhurtsilava@users.noreply.github.com>
2023-08-07 18:22:36 +0400

    Update App.jsx

--- Lecture11/my-app/src/App.jsx
+++ Lecture11/my-app/src/App.jsx
@@ -1,6 +1,11 @@
 
 import './App.css';
 import React from 'react';
+import gatsbyImage from './imgs/gatsby.jpeg'
+import hobitImage from './imgs/hobit.jpeg'
+import potterImage from './imgs/potter.jpeg'
+import prideImage from './imgs/Pride.jpeg'
+
 
 
 const books = [
@@ -9,28 +14,28 @@ const books = [
     author: "J.K. Rowling",
     characters: ["Harry Potter", "Hermione Granger", "Ron Weasley", "Albus Dumbledore"],
     shortIntro: "The first book in the iconic Harry Potter series, following the adventures of a young wizard, Harry Potter, and his friends at Hogwarts School of Witchcraft and Wizardry.",
-    coverPhoto: "./imgs/potter.jpeg"
+    coverPhoto: potterImage
   },
   {
     bookName: "The Hobbit",
     author: "J.R.R. Tolkien",
     characters: ["Bilbo Baggins", "Gandalf", "Thorin Oakenshield", "Smaug"],
     shortIntro: "An enchanting fantasy novel that takes readers on an epic adventure with Bilbo Baggins as he joins a group of dwarves on a quest to reclaim their homeland from the fearsome dragon Smaug.",
-    coverPhoto: "./imgs/hobit.jpeg"
+    coverPhoto: hobitImage
   },
   {
     bookName: "Pride and Prejudice",
     author: "Jane Austen",
     characters: ["Elizabeth Bennet", "Mr. Darcy", "Jane Bennet", "Mr. Bingley"],
     shortIntro: "A classic novel centered around the romantic entanglements of the Bennet sisters, particularly the headstrong Elizabeth Bennet and the proud Mr. Darcy.",
-    coverPhoto: "./imgs/Pride.jpeg"
+    coverPhoto: prideImage
   },
   {
     bookName: "The Great Gatsby",
     author: "F. Scott Fitzgerald",
     characters: ["Jay Gatsby", "Nick Carraway", "Daisy Buchanan", "Tom Buchanan"],
     shortIntro: "Set in the Roaring Twenties, this novel follows the mysterious Jay Gatsby's obsession with the elusive Daisy Buchanan, weaving a tale of wealth, decadence, and unfulfilled dreams.",
-    coverPhoto: "./imgs/gatsby.jpeg"
+    coverPhoto: gatsbyImage
   }
 ];
 
