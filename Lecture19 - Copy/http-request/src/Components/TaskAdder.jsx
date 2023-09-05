b4d16bcda006f3740643c5c253b88350ddf68293
Batu Khurtsilava <124504221+BatuKhurtsilava@users.noreply.github.com>
2023-09-05 18:34:28 +0400

    Add files via upload

---
+++ Lecture19 - Copy/http-request/src/Components/TaskAdder.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useTaskContext } from '../TaskContext';
+import useRequest from '../hooks/useRequest';
+
+
+const TaskAdder = () => {
+  const [checked, setChecked] = useState(false);
+  const [task, setTask] = useState('');
+  const navigate = useNavigate();
+  const url = '/api/v1/taskList';
+      
+  const {sendRequest} = useRequest({url: url, method: 'POST'});
+
+  const onFormSubmit = () => {
+
+    sendRequest([task, checked]).then(() => navigate('/'))
+    .catch(err => console.log(err));
+    
+  };
+
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onFormSubmit();
+  };
+
+  return (
+    <div>
+      <Link to = {'/'}>Back to tasks</Link>
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          placeholder='Enter Task'
+          onChange={(e) => setTask(e.target.value)}
+        />
+       
+          <input
+            type='checkbox'
+            checked={checked}
+            onChange={() => setChecked(!checked)}
+          />
+          Mark as completed
+        
+        <button type='submit'>Add Task</button>
+      </form>
+    </div>
+  );
+};
+
+export default TaskAdder;
---
+++ Lecture19 - Copy/http-request/src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  
+  return (
+    <div>
+      <header>
+        <Link to ></Link>
+      </header>
+    </div>
+  )
+}
